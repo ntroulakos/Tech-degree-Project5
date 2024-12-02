17b1fb62e5ed08148d234c57da539cc0d6f02ac2
Nikos Vasil <nikos.vasileiadis91@gmail.com>
2024-12-02 18:21:18 +0100

    Last changes
    Well done!

--- js/app.js
+++ js/app.js
@@ -1,25 +1,25 @@
-baguetteBox.run('.gallery');
-// Array of image data with src and alt text
-const imageData = [
-    { src: "img/01.jpg", alt: "Hay Bales, I love hay bales. Took this snap on a drive through the countryside past some straw fields." },
-    { src: "img/02.jpg", alt: "Lake, The lake was so calm today. We had a great view of the snow on the mountains from her" },
-    { src: "img/03.jpg", alt: "Canyon,  I hiked to the top of the mountain and got this picture of the canyon and trees below." },
-    { src: "img/04.jpg", alt: "Iceberg, It was amazing to see an iceberg up close, it was so cold but didn’t snow today. " },
-    { src: "img/05.jpg", alt: "Desert, The red cliffs were beautiful. It was really hot in the desert but we did a lot of walking through the canyons." },
-    { src: "img/06.jpg", alt: "Fall, Fall is coming, I love when the leaves on the trees start to change color." },
-    { src: "img/07.jpg", alt: "Plantation, I drove past this plantation yesterday, everything is so green!" },
-    { src: "img/08.jpg", alt: "Dunes, My summer vacation to the Oregon Coast. I love the sandy dunes!" },
-    { src: "img/09.jpg", alt: "Countryside Lane, We enjoyed a quiet stroll down this countryside lane. " },
-    { src: "img/10.jpg", alt: "Sunset, Sunset at the coast! The sky turned a lovely shade of orange." },
-    { src: "img/11.jpg", alt: "Cave, I did a tour of a cave today and the view of the landscape below was breathtaking." },
-    { src: "img/12.jpg", alt: "Bluebells, I walked through this meadow of bluebells and got a good view of the snow on the mountain before the fog came in." }
-];  
+baguetteBox.run('.grid', {
+    //These are the attributes that must be activated to make use of right, left and captions.
+    captions: true,  
+    leftArrow: '←',
+    rightArrow: '→',  
+    closeX: 'x'
+});
+//Grabing the id from the input
+const pictureSearch = document.getElementById('pictureSearch');
+//Checking the input by the user 
+pictureSearch.addEventListener('keyup', e => {
+    let currentValue = e.target.value.toLowerCase();
+    let pictures = document.querySelectorAll('img');
+//checking if the word matches a picture otherwise empty
+    pictures.forEach(picture => {
+        const altText = picture.alt.toLowerCase();
+        if (altText.includes(currentValue)){
+            picture.style.display = '';
+        }else {
+            picture.style.display = 'none';
+        }
 
-// let html = '';
-// for (let i=1; i <= 12; i++) {
-//      html += `<div>${i}</div>`;
-// }
-
-
-// document.querySelector('.grid').innerHTML = html;
+    })
+});
 
