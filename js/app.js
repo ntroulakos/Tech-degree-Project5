dde708f963beabec65ccca03b4cc065b0c660ac8
Nikos Vasil <nikos.vasileiadis91@gmail.com>
2024-12-02 16:06:37 +0100

    first commit
    first commit

---
+++ js/app.js
@@ -0,0 +1,25 @@
+baguetteBox.run('.gallery');
+// Array of image data with src and alt text
+const imageData = [
+    { src: "img/01.jpg", alt: "Hay Bales, I love hay bales. Took this snap on a drive through the countryside past some straw fields." },
+    { src: "img/02.jpg", alt: "Lake, The lake was so calm today. We had a great view of the snow on the mountains from her" },
+    { src: "img/03.jpg", alt: "Canyon,  I hiked to the top of the mountain and got this picture of the canyon and trees below." },
+    { src: "img/04.jpg", alt: "Iceberg, It was amazing to see an iceberg up close, it was so cold but didn’t snow today. " },
+    { src: "img/05.jpg", alt: "Desert, The red cliffs were beautiful. It was really hot in the desert but we did a lot of walking through the canyons." },
+    { src: "img/06.jpg", alt: "Fall, Fall is coming, I love when the leaves on the trees start to change color." },
+    { src: "img/07.jpg", alt: "Plantation, I drove past this plantation yesterday, everything is so green!" },
+    { src: "img/08.jpg", alt: "Dunes, My summer vacation to the Oregon Coast. I love the sandy dunes!" },
+    { src: "img/09.jpg", alt: "Countryside Lane, We enjoyed a quiet stroll down this countryside lane. " },
+    { src: "img/10.jpg", alt: "Sunset, Sunset at the coast! The sky turned a lovely shade of orange." },
+    { src: "img/11.jpg", alt: "Cave, I did a tour of a cave today and the view of the landscape below was breathtaking." },
+    { src: "img/12.jpg", alt: "Bluebells, I walked through this meadow of bluebells and got a good view of the snow on the mountain before the fog came in." }
+];  
+
+// let html = '';
+// for (let i=1; i <= 12; i++) {
+//      html += `<div>${i}</div>`;
+// }
+
+
+// document.querySelector('.grid').innerHTML = html;
+
